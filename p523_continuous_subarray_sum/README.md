ce064ff115ebf7399ea58ee480a70e14abe6135e
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-06-07 18:06:24 -0300

    p523_continuous_subarray_sum

---
+++ p523_continuous_subarray_sum/README.md
@@ -0,0 +1,33 @@
+# p523_continuous_subarray_sum
+[https://leetcode.com/problems/continuous-subarray-sum/](https://leetcode.com/problems/continuous-subarray-sum/)
+
+Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+- its length is at least two, and
+- the sum of the elements of the subarray is a multiple of k.
+
+Note that:
+
+- A subarray is a contiguous part of the array.
+- An integer x is a multiple of k if there exists an integer n such that $x = n * k$. 0 is always a multiple of k.
+
+## Initial provided code
+```Rust
+impl Solution {
+    pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
+        
+    }
+}
+```
+
+So, at this point I know:
+1. the parameter type; and
+2. the return type;
+
+## First approach - Hashing
+
+- n = number of elements
+- time complexity: O(n)
+- space complexity: O(1)
---
+++ p523_continuous_subarray_sum/src/lib.rs
@@ -0,0 +1,64 @@
+use std::collections::HashMap;
+struct Solution;
+impl Solution {
+    pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
+        let mut hash_map: HashMap<i32, i32> = HashMap::new();
+        hash_map.insert(0, 0);
+        let mut sum = 0;
+
+        for right_index in 0..nums.len() {
+            sum += nums[right_index];
+            if let Some(&left_index) = hash_map.get(&(sum % k)) {
+                if left_index < right_index as i32 {
+                    return true;
+                }
+            } else {
+                hash_map.insert(sum % k, right_index as i32 + 1);
+            }
+        }
+
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_01() {
+        let nums = vec![23, 2, 4, 6, 7];
+        let result1 = Solution::check_subarray_sum(nums, 6);
+        assert_eq!(result1, true);
+    }
+    // Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+
+    #[test]
+    fn case_02() {
+        let nums = vec![23, 2, 6, 4, 7];
+        let result1 = Solution::check_subarray_sum(nums, 6);
+        assert_eq!(result1, true);
+    }
+    // Explanation: [23, 2, 6, 4, 7] is an continuous subarray of size 5 whose elements sum up to 42. 42 is a multiple of 6 because 42 = 7 * 6 and 7 is an integer.
+
+    #[test]
+    fn case_03() {
+        let nums = vec![23, 2, 6, 4, 7];
+        let result1 = Solution::check_subarray_sum(nums, 13);
+        assert_eq!(result1, false);
+    }
+
+    #[test]
+    fn case_04() {
+        let nums = vec![2, 4, 3];
+        let result1 = Solution::check_subarray_sum(nums, 6);
+        assert_eq!(result1, true);
+    }
+
+    #[test]
+    fn case_05() {
+        let nums = vec![1, 3, 6, 0, 9, 6, 9];
+        let result1 = Solution::check_subarray_sum(nums, 7);
+        assert_eq!(result1, true);
+    }
+}
