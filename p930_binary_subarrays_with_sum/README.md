89dda805647a7c4c4db9820029b9f0958200f442
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-06-12 11:54:31 -0300

    p930_binary_subarrays_with_sum

---
+++ p930_binary_subarrays_with_sum/README.md
@@ -0,0 +1,17 @@
+# p930_binary_subarrays_with_sum
+[https://leetcode.com/problems/binary-subarrays-with-sum/](https://leetcode.com/problems/binary-subarrays-with-sum/)
+
+## Initial provided code
+```Rust
+impl Solution {
+    pub fn num_subarrays_with_sum(nums: Vec<i32>, goal: i32) -> i32 {
+        
+    }
+}
+```
+
+## First approach - Prefix + Hashmap
+
+- n = number of nodes
+- time complexity: $O(n)$
+- space complexity: $O(n)$
---
+++ p930_binary_subarrays_with_sum/src/lib.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+struct Solution;
+impl Solution {
+    pub fn num_subarrays_with_sum(nums: Vec<i32>, goal: i32) -> i32 {
+        let mut prefix = vec![0; nums.len() + 1];
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        let mut ans = 0;
+
+        for i in 0..nums.len() {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        for i in &prefix {
+            ans += map.get(&(i)).unwrap_or(&0);
+            *map.entry(*i + goal).or_insert(0) += 1;
+        }
+
+        ans
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_01() {
+        let nums = vec![1, 0, 1, 0, 1];
+        let result = Solution::num_subarrays_with_sum(nums, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn case_02() {
+        let nums = vec![0, 0, 0, 0, 0];
+        let result = Solution::num_subarrays_with_sum(nums, 0);
+        assert_eq!(result, 15);
+    }
+}
