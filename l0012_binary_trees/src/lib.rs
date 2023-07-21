2bb2b7df3f7d910bb1d5501f4fb5b6f773a9f34b
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-07-21 20:53:16 -0300

    l0012_binary_trees: from_vec_to_bt and from_bt_to_vec

--- l0012_binary_trees/src/lib.rs
+++ l0012_binary_trees/src/lib.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
@@ -16,6 +17,77 @@ impl TreeNode {
             right: None,
         }
     }
+}
+
+use std::ops::{Index, IndexMut};
+impl Index<usize> for TreeNode {
+    type Output = Option<Rc<RefCell<TreeNode>>>;
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.left,
+            1 => &self.right,
+            n => panic!("Invalid TreeNode index: {}", n),
+        }
+    }
+}
+
+impl IndexMut<usize> for TreeNode {
+    fn index_mut(&mut self, index: usize) -> &mut Option<Rc<RefCell<TreeNode>>> {
+        match index {
+            0 => &mut self.left,
+            1 => &mut self.right,
+            n => panic!("Invalid TreeNode index: {}", n),
+        }
+    }
+}
+
+fn from_vec_to_bt(vec: &[Option<i32>]) -> Option<Rc<RefCell<TreeNode>>> {
+    let tree = Rc::new(RefCell::new(TreeNode::new(vec.get(0)?.unwrap())));
+    let mut q = VecDeque::from([Rc::clone(&tree)]);
+    let mut i = 0;
+
+    while let Some(node) = q.pop_front() {
+        let mut node = node.borrow_mut();
+        for j in 0..=1 {
+            i += 1;
+            if let Some(&Some(val)) = vec.get(i) {
+                let new_node = Rc::new(RefCell::new(TreeNode::new(val)));
+                node[j] = Some(Rc::clone(&new_node));
+                q.push_back(new_node);
+            }
+        }
+    }
+
+    Some(tree)
+}
+
+fn from_bt_to_vec(root: &Option<Rc<RefCell<TreeNode>>>) -> Vec<Option<i32>> {
+    let mut result = Vec::new();
+    let mut q = VecDeque::new();
+
+    if let Some(root) = root {
+        q.push_back(Rc::clone(root));
+        result.push(Some(root.borrow().val));
+    }
+
+    while let Some(node) = q.pop_front() {
+        let node = node.borrow();
+
+        for i in 0..=1 {
+            if let Some(node) = &node[i] {
+                q.push_back(Rc::clone(node));
+                result.push(Some(node.borrow().val));
+            } else {
+                result.push(None);
+            }
+        }
+    }
+
+    while result[result.len() - 1].is_none() {
+        result.pop();
+    }
+
+    result
 }
 
 struct Solution;
@@ -123,5 +195,17 @@ mod tests {
         Solution::preorder_dfs(&Some(root.clone()));
         Solution::inorder_dfs(&Some(root.clone()));
         Solution::postorder_dfs(&Some(root.clone()));
+    }
+
+    #[test]
+    fn case_transformation() {
+        let vec1: Vec<Option<i32>> = vec![Some(0), None, Some(1), None, Some(2), None, Some(3)];
+        let bt_from_vec1: Option<Rc<RefCell<TreeNode>>> = from_vec_to_bt(&vec1);
+        let vec2: Vec<Option<i32>> = vec![Some(0), Some(1), None, Some(2), None, Some(3)];
+        let bt_from_vec2: Option<Rc<RefCell<TreeNode>>> = from_vec_to_bt(&vec2);
+        let vec1_from_bt = from_bt_to_vec(&bt_from_vec1);
+        let vec2_from_bt = from_bt_to_vec(&bt_from_vec2);
+        assert_eq!(vec1, vec1_from_bt);
+        assert_eq!(vec2, vec2_from_bt);
     }
 }
