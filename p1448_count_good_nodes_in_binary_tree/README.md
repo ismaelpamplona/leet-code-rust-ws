e3f7a4b37c5355cdb0286e44c48b5a7e21dc60cd
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-07-22 08:51:18 -0300

    (doing) p1448_count_good_nodes_in_binary_tree

---
+++ p1448_count_good_nodes_in_binary_tree/README.md
@@ -0,0 +1,38 @@
+# p1448_count_good_nodes_in_binary_tree
+[https://leetcode.com/problems/count-good-nodes-in-binary-tree/](https://leetcode.com/problems/count-good-nodes-in-binary-tree/)
+
+## Initial provided code
+```Rust
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn good_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        
+    }
+}
+```
+## First approach - Depth-first search (DFS) (recursion with a insider func)
+
+- n = number of nodes
+- time complexity: $O(n)$
+- space complexity: $O(log(n))$
+$
+
---
+++ p1448_count_good_nodes_in_binary_tree/src/lib.rs
@@ -0,0 +1,76 @@
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::ops::{Index, IndexMut};
+impl Index<usize> for TreeNode {
+    type Output = Option<Rc<RefCell<TreeNode>>>;
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.left,
+            1 => &self.right,
+            n => panic!("Invalid TreeNode index: {}", n),
+        }
+    }
+}
+
+impl IndexMut<usize> for TreeNode {
+    fn index_mut(&mut self, index: usize) -> &mut Option<Rc<RefCell<TreeNode>>> {
+        match index {
+            0 => &mut self.left,
+            1 => &mut self.right,
+            n => panic!("Invalid TreeNode index: {}", n),
+        }
+    }
+}
+
+fn from_vec_to_bt(vec: &[Option<i32>]) -> Option<Rc<RefCell<TreeNode>>> {
+    None
+}
+
+fn from_bt_to_vec(root: &Option<Rc<RefCell<TreeNode>>>) -> Vec<Option<i32>> {
+    let mut result: Vec<Option<i32>> = vec![];
+
+    result
+}
+
+struct Solution;
+impl Solution {
+    pub fn good_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_transformation() {
+        let vec1: Vec<Option<i32>> = vec![Some(0), None, Some(1), None, Some(2), None, Some(3)];
+        let bt_from_vec1: Option<Rc<RefCell<TreeNode>>> = from_vec_to_bt(&vec1);
+        let vec2: Vec<Option<i32>> = vec![Some(0), Some(1), None, Some(2), None, Some(3)];
+        let bt_from_vec2: Option<Rc<RefCell<TreeNode>>> = from_vec_to_bt(&vec2);
+        let vec1_from_bt = from_bt_to_vec(&bt_from_vec1);
+        let vec2_from_bt = from_bt_to_vec(&bt_from_vec2);
+        assert_eq!(vec1, vec1_from_bt);
+        assert_eq!(vec2, vec2_from_bt);
+    }
+}
