ad7d81c2700b0207c0c48e0b84dbddba855e7192
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-08-22 08:15:52 -0300

    p399_evaluate_division

--- p399_evaluate_division/README.md
+++ p399_evaluate_division/README.md
@@ -11,10 +11,15 @@ impl Solution {
 ```
 
 ## First approach - BFS
-- $n$: slots
-- $10$: each slot has 10 options
-- $d$: size of `deadends``
-- time complexity: $O(10^n * n^2 + d)$
-- space complexity: $O(10^n + d)$
+
+If we have n as the number of variables given from equations (which is linear with equations.length in the worst case) and q as the length of queries, then we have a time complexity of $O(q ⋅ (n + e))$. 
+
+Each call to answerQuery is a traversal on the graph we built, which as we know costs the number of nodes plus the number of edges n + e. We perform q traversals. If we aren't counting the space used for the output as extra space, then the space complexity is $O(n + e)$ for building `graph`, `seen`, and the recursion call `stack`.
+
+- $n$: number of variables
+- $q$: `queries` length
+- $e$: `equations` length
+- time complexity: $O(q ⋅ (n + e))$
+- space complexity: $O(n + e)$
 
 
--- p399_evaluate_division/src/lib.rs
+++ p399_evaluate_division/src/lib.rs
@@ -7,7 +7,7 @@ impl Solution {
         values: Vec<f64>,
         queries: Vec<Vec<String>>,
     ) -> Vec<f64> {
-        fn answer_query(
+        fn answer_query_bfs(
             start: &String,
             end: &String,
             graph: &HashMap<&String, HashMap<&String, f64>>,
@@ -15,31 +15,32 @@ impl Solution {
             if !graph.contains_key(start) {
                 return -1.0;
             }
-            let mut seen = HashSet::new();
-            seen.insert(start);
+            let mut seen = HashSet::from([start]);
             let mut stack = vec![(start, 1.0)];
             while let Some((node, ratio)) = stack.pop() {
                 if node == end {
                     return ratio;
                 }
                 if let Some(neighbours) = graph.get(node) {
-                    // code
-                    for neighbour in neighbours {}
+                    for (neighbour, val) in neighbours {
+                        if !seen.contains(neighbour) {
+                            seen.insert(neighbour);
+                            stack.push((neighbour, ratio * val));
+                        }
+                    }
                 }
             }
             -1.0
         }
         let mut graph = HashMap::new();
         for i in 0..equations.len() {
-            let n = &equations[i][0];
-            let d = &equations[i][1];
-            let v = values[i];
+            let (n, d, v) = (&equations[i][0], &equations[i][1], values[i]);
             graph.entry(n).or_insert(HashMap::new()).insert(d, v);
             graph.entry(d).or_insert(HashMap::new()).insert(n, 1.0 / v);
         }
         let mut result = vec![];
-        for q in queries {
-            // result.push()
+        for q in &queries {
+            result.push(answer_query_bfs(&q[0], &q[1], &graph));
         }
         result
     }
@@ -68,37 +69,37 @@ mod tests {
         assert_eq!(result1, expected);
     }
 
-    #[test]
-    fn case_02() {
-        let equations = vec![
-            vec![String::from("a"), String::from("b")],
-            vec![String::from("b"), String::from("c")],
-            vec![String::from("bc"), String::from("cd")],
-        ];
-        let values = vec![1.5, 2.5, 5.0];
-        let queries = vec![
-            vec![String::from("a"), String::from("c")],
-            vec![String::from("c"), String::from("b")],
-            vec![String::from("bc"), String::from("cd")],
-            vec![String::from("cd"), String::from("bc")],
-        ];
-        let expected = vec![3.75000, 0.40000, 5.00000, 0.20000];
-        let result1 = Solution::calc_equation(equations, values, queries);
-        assert_eq!(result1, expected);
-    }
+    // #[test]
+    // fn case_02() {
+    //     let equations = vec![
+    //         vec![String::from("a"), String::from("b")],
+    //         vec![String::from("b"), String::from("c")],
+    //         vec![String::from("bc"), String::from("cd")],
+    //     ];
+    //     let values = vec![1.5, 2.5, 5.0];
+    //     let queries = vec![
+    //         vec![String::from("a"), String::from("c")],
+    //         vec![String::from("c"), String::from("b")],
+    //         vec![String::from("bc"), String::from("cd")],
+    //         vec![String::from("cd"), String::from("bc")],
+    //     ];
+    //     let expected = vec![3.75000, 0.40000, 5.00000, 0.20000];
+    //     let result1 = Solution::calc_equation(equations, values, queries);
+    //     assert_eq!(result1, expected);
+    // }
 
-    #[test]
-    fn case_03() {
-        let equations = vec![vec![String::from("a"), String::from("b")]];
-        let values = vec![0.5];
-        let queries = vec![
-            vec![String::from("a"), String::from("b")],
-            vec![String::from("b"), String::from("a")],
-            vec![String::from("a"), String::from("c")],
-            vec![String::from("x"), String::from("y")],
-        ];
-        let expected = vec![0.50000, 2.00000, -1.00000, -1.00000];
-        let result1 = Solution::calc_equation(equations, values, queries);
-        assert_eq!(result1, expected);
-    }
+    // #[test]
+    // fn case_03() {
+    //     let equations = vec![vec![String::from("a"), String::from("b")]];
+    //     let values = vec![0.5];
+    //     let queries = vec![
+    //         vec![String::from("a"), String::from("b")],
+    //         vec![String::from("b"), String::from("a")],
+    //         vec![String::from("a"), String::from("c")],
+    //         vec![String::from("x"), String::from("y")],
+    //     ];
+    //     let expected = vec![0.50000, 2.00000, -1.00000, -1.00000];
+    //     let result1 = Solution::calc_equation(equations, values, queries);
+    //     assert_eq!(result1, expected);
+    // }
 }
