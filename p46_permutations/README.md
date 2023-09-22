f00e57cf42b642778f4339bd5a0030f2840a8975
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-09-22 13:01:03 -0300

    p46_permutations

---
+++ p46_permutations/README.md
@@ -0,0 +1,25 @@
+# p46_permutations
+[https://leetcode.com/problems/permutations/](https://leetcode.com/problems/permutations/)
+
+## Initial provided code
+```Rust
+impl Solution {
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        
+    }
+}
+```
+## First approach - Backtracking
+
+- n = number of numbers
+
+### time complexity: aprox. $O(n \cdot n!)$
+
+- The initial call to backtrack (the "root" of the tree), makes `n` calls.
+- Each of those calls makes `n - 1` calls (avoiding duplicates), and each of those make `n - 2`, and so on. The exact time complexity of the algorithm is actually quite complicated, but we can use the logic just stated to estimate it to be around $O(n \cdot n!)$.
+
+### space complexity: $O(n)$
+- There is an extra factor of n because we need to copy each permutation. The space complexity is $O(n)$ for `cur` and the recursion call stack.
+
+
+
---
+++ p46_permutations/src/lib.rs
@@ -0,0 +1,57 @@
+struct Solution;
+impl Solution {
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        fn backtrack(cur: &mut Vec<i32>, nums: &[i32], ans: &mut Vec<Vec<i32>>) {
+            if cur.len() == nums.len() {
+                ans.push(cur.to_vec());
+                return;
+            }
+            for n in nums {
+                if !cur.contains(n) {
+                    cur.push(*n);
+                    backtrack(cur, nums, ans);
+                    cur.pop();
+                }
+            }
+        }
+        let mut ans: Vec<Vec<i32>> = vec![];
+        backtrack(&mut vec![], &nums, &mut ans);
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_01() {
+        let nums = vec![1, 2, 3];
+        let result = Solution::permute(nums);
+        let expected = vec![
+            vec![1, 2, 3],
+            vec![1, 3, 2],
+            vec![2, 1, 3],
+            vec![2, 3, 1],
+            vec![3, 1, 2],
+            vec![3, 2, 1],
+        ];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn case_02() {
+        let nums = vec![0, 1];
+        let result = Solution::permute(nums);
+        let expected = vec![vec![0, 1], vec![1, 0]];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn case_03() {
+        let nums = vec![1];
+        let result = Solution::permute(nums);
+        let expected = vec![vec![1]];
+        assert_eq!(result, expected);
+    }
+}
