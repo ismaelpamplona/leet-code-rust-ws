b131a7d83ec8484185e091b387c11627e359d49e
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-01-17 14:30:49 -0300

    (up) Issue #p42_trapping_rain_water.

---
+++ p42_trapping_rain_water/README.md
@@ -0,0 +1,23 @@
+# p42_trapping_rain_water
+
+[https://leetcode.com/problems/trapping-rain-water/](https://leetcode.com/problems/trapping-rain-water/)
+
+## Initial provided code
+
+```Rust
+impl Solution {
+    pub fn trap(height: Vec<i32>) -> i32 {
+
+    }
+}
+```
+
+## First approach - Two Pointers
+
+- time complexity: $O(n)$
+- space complexity: $O(n)$
+
+## Second approach - Two Pointers
+
+- time complexity: $O(n)$
+- space complexity: $O(1)$
---
+++ p42_trapping_rain_water/src/lib.rs
@@ -0,0 +1,53 @@
+struct Solution;
+impl Solution {
+    pub fn trap_a(height: Vec<i32>) -> i32 {
+        let n = height.len();
+        let mut max_left = vec![0; n];
+        let mut max_right = vec![0; n];
+        let mut cur_max_left = 0;
+        let mut cur_max_right = 0;
+        for i in 1..n {
+            cur_max_left = cur_max_left.max(height[i - 1]);
+            max_left[i] = cur_max_left;
+            cur_max_right = cur_max_right.max(height[n - i]);
+            max_right[n - i - 1] = cur_max_right;
+        }
+        let mut min_left_right = vec![0; n];
+        let mut max_vol = 0;
+        for i in 0..n {
+            min_left_right[i] = max_left[i].min(max_right[i]);
+            let max_trap = min_left_right[i] - height[i];
+            if max_trap >= 0 {
+                max_vol += max_trap;
+            }
+        }
+        max_vol
+    }
+
+    pub fn trap_b(height: Vec<i32>) -> i32 {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_01() {
+        let height = vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+        let result1 = Solution::trap_a(height.clone());
+        let result2 = Solution::trap_b(height.clone());
+        assert_eq!(result1, 6);
+        assert_eq!(result2, 6);
+    }
+
+    #[test]
+    fn case_02() {
+        let height = vec![4, 2, 0, 3, 2, 5];
+        let result1 = Solution::trap_a(height.clone());
+        let result2 = Solution::trap_b(height.clone());
+        assert_eq!(result1, 9);
+        assert_eq!(result2, 9);
+    }
+}
